7736087405d23fa9d511636b2884a0aeb6a84c64
kschreiber <klemens.schreiber@karriere.at>
2016-02-13 17:55:28 +0100

    systemfont stack

--- main.js
+++ main.js
@@ -9,21 +9,6 @@ if (Meteor.isClient) {
         var usernameSeeds = ['yadda', 'yolo', 'raised fist'];
         var randomUsername = usernameSeeds[Math.floor(Math.random()*usernameSeeds.length)];
         Session.set('username', randomUsername);
-
-        WebFontConfig = {
-            google: {
-                families: ['Hind+Siliguri:400,500,300,700,600:latin']
-            }
-        };
-        (function() {
-            var wf = document.createElement('script');
-            wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-                '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-            wf.type = 'text/javascript';
-            wf.async = 'true';
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(wf, s);
-        })();
     });
 }
 
